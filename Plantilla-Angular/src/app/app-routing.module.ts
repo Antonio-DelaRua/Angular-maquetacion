345e59422f0e300d70f3b065d03185baae349e06
Antonio-DelaRua <ruxxdeveloper@gmail.com>
2022-09-30 16:13:23 +0200

    galeria

--- Plantilla-Angular/src/app/app-routing.module.ts
+++ Plantilla-Angular/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateCarComponent } from './pages/create-car/create-car.component';
+import { GaleriaComponent } from './pages/galeria/galeria.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   {path:'', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'create', component: CreateCarComponent},
+  {path:'galery', component: GaleriaComponent},
   {path: '**', component: NotFoundComponent}
 ];
 
--- Plantilla-Angular/src/app/app.module.ts
+++ Plantilla-Angular/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { CreateCarComponent } from './pages/create-car/create-car.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { GaleriaComponent } from './pages/galeria/galeria.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { FooterComponent } from './components/footer/footer.component';
     NotFoundComponent,
     CreateCarComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    GaleriaComponent
   ],
   imports: [
     BrowserModule,
